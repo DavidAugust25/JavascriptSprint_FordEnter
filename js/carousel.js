f070982f7c39928c76fa22fb61b226498bdeb7a7
David_o_gostoso <davidonilson2574@gmail.com>
2026-05-26 14:31:53 -0300

    working on the css

--- js/carousel.js
+++ js/carousel.js
@@ -15,47 +15,57 @@ class Carousel {
     }  
 
         static Start(arr){
-        if(arr){
-            
-            if(arr.length > 0){
-                Carousel._sequence = 0;
-                Carousel._size = arr.length;
-                Carousel.Next(); //começar
-                Carousel._interval = setInterval(function(){ Carousel.Next(); },8000);
-            }
-            
+        if(arr && arr.length > 0){
+            Carousel._sequence = 0;
+            Carousel._size = arr.length;
+            Carousel.Render(); // Mostrar o primeiro slide imediatamente
+            Carousel.ResetTimer();
         } else {
-            throw "Método necessita de uma Variável pro Conjunto.";
+            throw "Método necessita de uma Variável pro Conjunto válido.";
         }
     }
 
+    static Render() {
+        let carouselItem = carouselArr[Carousel._sequence];
+        let imagemdiv = document.getElementById("carousel");
+        let titulodiv = document.getElementById("carousel-title");
+
+        if (!imagemdiv || !titulodiv) return; // Segurança caso a DOM não esteja pronta
+
+        imagemdiv.style.backgroundImage = `url('img/${carouselItem.image}')`;
+        imagemdiv.style.backgroundSize = "cover";
+        imagemdiv.style.backgroundPosition = "center";
+        imagemdiv.style.transition = "all .8s ease-in-out";
+        imagemdiv.innerHTML = `<a href="${carouselItem.url}" style="display:block; width:100%; height:100%;"></a>`;
+
+        titulodiv.innerHTML = `<a href="${carouselItem.url}">${carouselItem.title}</a>`;
+    }
+
     static Next(){
-       let carouselItem = carouselArr[Carousel._sequence];
-       let imagemdiv = document.getElementById("carousel");
-       let linkdiv = document.getElementById("carousel-link");
-       let titulodiv = document.getElementById("carousel-title");
-
-       imagemdiv.style.backgroundImage = `url('img/${carouselItem.image}')`;
-       imagemdiv.style.backgroundSize = "cover";
-       imagemdiv.style.backgroundPosition = "center";
-       imagemdiv.style.transition = "all .8s ease-in-out";
-       imagemdiv.innerHTML = `<a href="${carouselItem.url}"></a>`;
-
-       titulodiv.innerHTML = `<a href="${carouselItem.url}">${carouselItem.title}</a>`;
-
-       Carousel._sequence++;
-
-       if (Carousel._sequence>=Carousel._size) {
-           Carousel._sequence = 0;
-       }
-    }
-    static nextSlide() {
-        carouselItem = carouselArr[Carousel._sequence];
         Carousel._sequence++;
+        if (Carousel._sequence >= Carousel._size) {
+            Carousel._sequence = 0;
+        }
+        Carousel.Render();
     }
+
+    static ResetTimer() {
+        clearInterval(Carousel._interval);
+        Carousel._interval = setInterval(function(){ Carousel.Next(); }, 8000);
+    }
+
+    static nextSlide() {
+        Carousel.Next();
+        Carousel.ResetTimer();
+    }
+
     static prevSlide() {
-        carouselItem = carouselArr[Carousel._sequence];
         Carousel._sequence--;
+        if (Carousel._sequence < 0) {
+            Carousel._sequence = Carousel._size - 1; 
+        }
+        Carousel.Render();
+        Carousel.ResetTimer(); 
     }
 };
 
